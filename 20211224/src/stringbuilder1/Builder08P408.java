edb77c0a6c974601526282d39ebf4fbf58682d5a
jjjjjjji-hun <lingcke@naver.com>
2021-12-24 15:40:00 +0900

    21년 12월 24일 String 클래스

---
+++ 20211224/src/stringbuilder1/Builder08P408.java
@@ -0,0 +1,17 @@
+package stringbuilder1;
+
+public class Builder08P408 {
+
+	public static void main(String[] args) {
+		// reverse()는 문자열을 인덱스 순으로 따졌을떄
+		// 역순으로 나열해줍니다.
+		
+		StringBuilder str = new StringBuilder("안녕하세요 김자바입니다.");
+		
+		str.reverse();
+		
+		System.out.println(str);
+
+	}
+
+}
